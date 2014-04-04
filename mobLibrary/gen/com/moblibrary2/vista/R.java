b77c15e89296eb636acfcfccbc7cdce603a25ca2
Esteban Segura <esteban.1703@gmail.com>
2014-04-03 18:18:54 -0600

    La parte de los spinners ya esta listo, para cargar la informacion de los spinners utilizo una clase "dummy" se espera que ahi esta la informacion que se va traer de la base de datos Fecha 4/3/2014

--- mobLibrary/gen/com/moblibrary2/vista/R.java
+++ mobLibrary/gen/com/moblibrary2/vista/R.java
@@ -8,6 +8,9 @@
 package com.moblibrary2.vista;
 
 public final class R {
+    public static final class array {
+        public static final int tipo_busqueda=0x7f040000;
+    }
     public static final class attr {
     }
     public static final class dimen {
@@ -17,32 +20,32 @@ public final class R {
          screen margins) for sw720dp devices (e.g. 10" tablets) in landscape here.
     
          */
-        public static final int activity_horizontal_margin=0x7f040000;
-        public static final int activity_vertical_margin=0x7f040001;
+        public static final int activity_horizontal_margin=0x7f050000;
+        public static final int activity_vertical_margin=0x7f050001;
     }
     public static final class drawable {
         public static final int ic_launcher=0x7f020000;
     }
     public static final class id {
-        public static final int action_settings=0x7f080012;
-        public static final int btnAtras=0x7f080000;
-        public static final int btnBuscar=0x7f080008;
-        public static final int btnEditar=0x7f080010;
-        public static final int btnProximasLecturas=0x7f08000e;
-        public static final int btnRecomendar=0x7f08000f;
-        public static final int btnSalir=0x7f080011;
-        public static final int btnUbicar=0x7f08000d;
-        public static final int lblBuscar=0x7f080006;
-        public static final int lblLibreria=0x7f080002;
-        public static final int lblPass=0x7f08000b;
-        public static final int lblTipoBuscar=0x7f080004;
-        public static final int lblTitulo=0x7f080001;
-        public static final int lblUsuario=0x7f080009;
-        public static final int spnLibreria=0x7f080003;
-        public static final int spnTipoBusqueda=0x7f080005;
-        public static final int txtBuscar=0x7f080007;
-        public static final int txtPass=0x7f08000c;
-        public static final int txtUsuario=0x7f08000a;
+        public static final int action_settings=0x7f090012;
+        public static final int btnAtras=0x7f090000;
+        public static final int btnBuscar=0x7f090008;
+        public static final int btnEditar=0x7f090010;
+        public static final int btnProximasLecturas=0x7f09000e;
+        public static final int btnRecomendar=0x7f09000f;
+        public static final int btnSalir=0x7f090011;
+        public static final int btnUbicar=0x7f09000d;
+        public static final int lblBuscar=0x7f090006;
+        public static final int lblLibreria=0x7f090002;
+        public static final int lblPass=0x7f09000b;
+        public static final int lblTipoBuscar=0x7f090004;
+        public static final int lblTitulo=0x7f090001;
+        public static final int lblUsuario=0x7f090009;
+        public static final int spnLibreria=0x7f090003;
+        public static final int spnTipoBusqueda=0x7f090005;
+        public static final int txtBuscar=0x7f090007;
+        public static final int txtPass=0x7f09000c;
+        public static final int txtUsuario=0x7f09000a;
     }
     public static final class layout {
         public static final int activity_buscar=0x7f030000;
@@ -50,27 +53,27 @@ public final class R {
         public static final int activty_menu=0x7f030002;
     }
     public static final class menu {
-        public static final int activity_principal=0x7f070000;
+        public static final int activity_principal=0x7f080000;
     }
     public static final class string {
-        public static final int action_settings=0x7f050001;
-        public static final int app_name=0x7f050000;
-        public static final int atras=0x7f05000e;
-        public static final int buscar=0x7f050008;
-        public static final int contrasena=0x7f050006;
-        public static final int editar=0x7f05000c;
-        public static final int entrar=0x7f050007;
-        public static final int hello_world=0x7f050002;
-        public static final int libreria=0x7f050010;
-        public static final int nombre_aplicacion=0x7f050003;
-        public static final int proximo=0x7f05000a;
-        public static final int recomendar=0x7f05000b;
-        public static final int salir=0x7f05000d;
-        public static final int space=0x7f050004;
-        public static final int tipoBuscar=0x7f050011;
-        public static final int titulobuscar=0x7f05000f;
-        public static final int ubicar=0x7f050009;
-        public static final int usuario=0x7f050005;
+        public static final int action_settings=0x7f060001;
+        public static final int app_name=0x7f060000;
+        public static final int atras=0x7f06000e;
+        public static final int buscar=0x7f060008;
+        public static final int contrasena=0x7f060006;
+        public static final int editar=0x7f06000c;
+        public static final int entrar=0x7f060007;
+        public static final int hello_world=0x7f060002;
+        public static final int libreria=0x7f060010;
+        public static final int nombre_aplicacion=0x7f060003;
+        public static final int proximo=0x7f06000a;
+        public static final int recomendar=0x7f06000b;
+        public static final int salir=0x7f06000d;
+        public static final int space=0x7f060004;
+        public static final int tipoBuscar=0x7f060011;
+        public static final int titulobuscar=0x7f06000f;
+        public static final int ubicar=0x7f060009;
+        public static final int usuario=0x7f060005;
     }
     public static final class style {
         /** 
@@ -94,10 +97,10 @@ public final class R {
     
  API 14 theme customizations can go here. 
          */
-        public static final int AppBaseTheme=0x7f060000;
+        public static final int AppBaseTheme=0x7f070000;
         /**  Application theme. 
  All customizations that are NOT specific to a particular API-level can go here. 
          */
-        public static final int AppTheme=0x7f060001;
+        public static final int AppTheme=0x7f070001;
     }
 }
--- mobLibrary/src/com/moblibrary2/vista/ActivityBuscar.java
+++ mobLibrary/src/com/moblibrary2/vista/ActivityBuscar.java
@@ -1,12 +1,69 @@
 package com.moblibrary2.vista;
 
+import java.util.ArrayList;
+
 import android.app.Activity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.AdapterView.OnItemSelectedListener;
+import android.widget.ArrayAdapter;
+import android.widget.Spinner;
+import android.widget.Toast;
+
+import com.moblibrary2.modelo.AdapterLibreria;
+import com.moblibrary2.modelo.Libreria;
+import com.moblibrary2.modelo.TestDBO;
 
 public class ActivityBuscar extends Activity {
+	
+	private Spinner spnLibreria;
+	private Spinner spnTipoBusqueda;
+	
+	AdapterLibreria adapter_libreria;
+	ArrayAdapter<?> adapter_tipo_busqueda;
+	TestDBO test_DBO = new TestDBO();
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_buscar);
+		
+		/*e llenana los valores a mostrar del spnLibreria en este caso los valores son traidos de base de datos a la que se 
+		 encuentra asociado la aplicacion*/
+		ArrayList<Libreria> librerias = test_DBO.getLibrerias();
+		
+		adapter_libreria = new AdapterLibreria(this, android.R.layout.simple_spinner_item, librerias);
+
+		spnLibreria = (Spinner) findViewById(R.id.spnLibreria);
+		spnLibreria.setAdapter(adapter_libreria);
+	
+		spnLibreria.setOnItemSelectedListener(new OnItemSelectedListener() {
+		
+			public void onItemSelected(AdapterView<?> parentView, View selectedItemViewed,
+					int position, long id){
+				Libreria libreria = adapter_libreria.getItem(position);
+				Toast.makeText(ActivityBuscar.this, "ID: " + libreria.getId() + "\nNombre: "
+				+ libreria.getNombreLibreria(), Toast.LENGTH_SHORT).show();
+				
+			}
+			
+			public void onNothingSelected(AdapterView<?> parentView) {
+			
+			}
+		});
+		
+		/*Se llenana los valores a mostrar del spnTipoBusqueda en este caso los valores son traidos de la carpeta res del proyecto
+		del documento XML array_buscar_por*/
+		spnTipoBusqueda = (Spinner) findViewById(R.id.spnTipoBusqueda);
+		adapter_tipo_busqueda = ArrayAdapter.createFromResource(
+				this, R.array.tipo_busqueda,android.R.layout.simple_spinner_dropdown_item);
+		adapter_tipo_busqueda.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+		spnTipoBusqueda.setAdapter(adapter_tipo_busqueda);
+		
+		
+		
+		
+			
 	}
 }
